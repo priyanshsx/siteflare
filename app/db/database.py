2543400b3294d819b37a7baedd72fdf8e550e670
Priyansh <saxenapriyansh95@gmail.com>
2026-09-25 14:22:18 +0530

    updated database.py

--- app/db/database.py
+++ app/db/database.py
@@ -2,4 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.orm import declarative_base 
 
+DATABASE_URL = 
+
+engine = create_async_engine(DATABASE_URL, )
 
