496fde4864e83ef5ebec6ee3e8716ec0ca2fb711
Priyansh <saxenapriyansh95@gmail.com>
2026-09-25 18:01:39 +0530

    expanded the audit file

--- app/models/audit.py
+++ app/models/audit.py
@@ -1,3 +1,12 @@
 from sqlalchemy import Column, Integer, String, DateTime, JSON
 from app.db.database import base
 
+class AuditLog(base):
+
+    __tablename__ = 'auditlogs'
+
+    id = Column(Integer, primary_key=True)
+    user_email = Column(String)
+    target_url = Column(String)
+    created_at = Column(DateTime)
+    audit_data = Column(JSON)
